fccc7584dba05b5275904fbd34bd59dbd816ba0f
Jhbalza2002 <josehumberto2002@gmail.com>
2024-06-18 18:58:55 -0500

    React router page functional

---
+++ React_Router_Project/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import MainContainer from "./components/MainContainer";
+function App() {
+  return (
+    <>
+      <div id="container">
+        <Navbar />
+        <MainContainer />
+      </div>
+    </>
+  );
+}
+
+export default App;
